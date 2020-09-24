53e925e581f6c8b2e097505fe406cc2dd5b65d9b
Rostislav Kremecek <rostislav.kremecek@gmail.com>
2020-09-24 15:06:54 +0200

    init commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var MY_NODE_VERSION = process.version;
+console.log("Node.js version: " + MY_NODE_VERSION);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const MY_NODE_VERSION: string = process.version;
+
+console.log(`Node.js version: ${MY_NODE_VERSION}`);
