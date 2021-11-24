86235670029bc37bd1728cca73d62d06ad9ea436
rixcian <rostislav.kremecek@protonmail.com>
2021-11-24 19:08:02 +0100

    feature: add .env support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
 const MY_NODE_VERSION: string = process.version;
 
+console.clear();
+console.log(`Hello, ${process.env.TEST_ENV}!`);
 console.log(`Node.js version: ${MY_NODE_VERSION}`);
